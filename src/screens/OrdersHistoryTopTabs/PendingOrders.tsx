56564ff0f9a2216d3bb637bdc3f7a5849d03941d
Imtanan Aziz Toor <imtanan@smash.cloud>
2023-08-18 18:23:43 +0500

    Pending delviery label changes

--- src/screens/OrdersHistoryTopTabs/PendingOrders.tsx
+++ src/screens/OrdersHistoryTopTabs/PendingOrders.tsx
@@ -1,7 +1,20 @@
+import { useSelector } from "react-redux";
 import OrdersHistoryList from "../../components/organism/OrdersHistoryList";
 import dataConstants from "../../constants/dataConstants";
+import { useNavigation } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
 
 function PendingOrders() {
+  const { mode } = useSelector((state: any) => state.Auth)
+  const navigation: any = useNavigation()
+
+  useLayoutEffect(() => {
+    if (mode === 'vendor') {
+      navigation.setOptions({
+        title: 'Pending Delivery'
+      })
+    }
+  }, [])
   return <OrdersHistoryList filterStatus={dataConstants.orderStatus.CONFIRMED} />
 }
 
